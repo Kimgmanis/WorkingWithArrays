5385e23b9f82d70875040bfa40fed928033434bb
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-26 21:23:17 +0200

    ini

--- WorkingWithArrays/Program.cs
+++ WorkingWithArrays/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var episodes = new (int season, int episode, string title, int length)[]
+{ // Ini
+    (1, 1, "Simpsons Roasting on an Open Fire", 22),
+    (1, 2, "Bart the Genius", 22),
+    (1, 3, "Homer's Odyssey", 22),
+    (1, 4, "There's No Disgrace Like Home", 23),
+    (1, 5, "Bart the General", 22),
+    (1, 6, "Moaning Lisa", 22),
+    (1, 7, "The Call of the Simpsons", 22),
+    (1, 8, "The Telltale Head", 22),
+    (1, 9, "Life on the Fast Lane", 22),
+    (1, 10, "Homer's Night Out", 22),
+};
